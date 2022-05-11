9ef0aeeace7ca7ef2b65e3f48ed85587949e9aac
Guilherme Canella <guycanella@gmail.com>
2022-05-11 20:33:17 -0300

    install and config firebase

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
 import AppLoading from 'expo-app-loading'
 
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+
+import { ThemeProvider }  from 'styled-components/native'
+import theme from './src/theme'
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ DMSans_400Regular, DMSerifDisplay_400Regular })
@@ -11,8 +13,8 @@ export default function App() {
 	if (!fontsLoaded) return <AppLoading />
 
   return (
-    <View>
-    </View>
+    <ThemeProvider theme={theme} >
+    </ThemeProvider>
   );
 }
 
