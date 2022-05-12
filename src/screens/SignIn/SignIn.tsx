18387c09fabcb1b081f1d75c98549cbb8cbcbb93
Guilherme Canella <guycanella@gmail.com>
2022-05-12 20:48:27 -0300

    finalization of signin

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,23 +1,45 @@
 import React from 'react'
+import { KeyboardAvoidingView, Platform } from 'react-native'
+
+import brandImg from '@assets/brand.png'
 
 import Input from '@components/Input'
 import Button from '@components/Button'
 
-import { Container } from './styles'
+import {
+	Brand,
+	Container,
+	Content,
+	ForgotPasswordButton,
+	ForgotPasswordLabel,
+	Title
+} from './styles'
 
 const SignIn = () => {
 	return (
 		<Container>
-			<Input
-				placeholder='E-mail'
-				type='secondary'
-				autoCorrect={false}
-				autoCapitalize='none'
-			/>
+			<KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+				<Content>
+					<Brand  source={brandImg} />
+					<Title>Login</Title>
+					<Input
+						placeholder='E-mail'
+						type='secondary'
+						autoCorrect={false}
+						autoCapitalize='none'
+					/>
 
-			<Input placeholder='Senha' type='secondary' secureTextEntry />
+					<Input placeholder='Senha' type='secondary' secureTextEntry />
 
-			<Button title="Entrar" type='secondary' />
+					<ForgotPasswordButton>
+						<ForgotPasswordLabel>
+							Esqueci minha senha
+						</ForgotPasswordLabel>
+					</ForgotPasswordButton>
+
+					<Button title="Entrar" type='secondary' />
+				</Content>
+			</KeyboardAvoidingView>
 		</Container>
 	)
 }
