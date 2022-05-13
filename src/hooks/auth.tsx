986311d95d999133ef0430bc44d048f8c3d78f2a
Guilherme Canella <guycanella@gmail.com>
2022-05-12 22:47:17 -0300

    implementing authentication

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,11 +1,16 @@
 import React,
 	{ createContext,
 		useContext,
-		ReactNode
+		ReactNode,
+		useState
 } from 'react'
+import { Alert } from 'react-native'
 
-type AuthContextData = {
+import auth from '@react-native-firebase/auth'
 
+export type AuthContextData = {
+	signIn: (email: string, password: string) => Promise<void>
+	isLogging: boolean
 }
 
 type AuthProviderProps = {
@@ -15,8 +20,35 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData)
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
+	const [isLogging, setIsLogging] = useState(false)
+
+	const signIn = async (email: string, password: string) => {
+		if (!email || !password) {
+			return Alert.alert('Login', 'Informe o e-mail e a senha.')
+		}
+
+		setIsLogging(true)
+
+		auth().signInWithEmailAndPassword(email, password)
+			.then(account => {
+				console.log('oi')
+			})
+			.catch(error => {
+				const { code } = error
+
+				if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+					return Alert.alert('Login', 'E-mail e/ou senha inválida.')
+				}
+
+				return Alert.alert('Login', 'Não foi possível realizar o login.')
+			})
+			.finally(() => {
+				setIsLogging(false)
+			})
+	}
+
 	return (
-		<AuthContext.Provider value={{}}>
+		<AuthContext.Provider value={{ signIn, isLogging}}>
 			{children}
 		</AuthContext.Provider>
 	)
--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { KeyboardAvoidingView, Platform } from 'react-native'
+
+import { useAuth } from '@hooks/auth'
 
 import brandImg from '@assets/brand.png'
 
@@ -16,6 +18,14 @@ import {
 } from './styles'
 
 const SignIn = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const { signIn, isLogging } = useAuth()
+
+	const handleSignIn = () => {
+		signIn(email, password)
+	}
+
 	return (
 		<Container>
 			<KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
@@ -27,9 +37,15 @@ const SignIn = () => {
 						type='secondary'
 						autoCorrect={false}
 						autoCapitalize='none'
+						onChangeText={setEmail}
 					/>
 
-					<Input placeholder='Senha' type='secondary' secureTextEntry />
+					<Input
+						placeholder='Senha'
+						type='secondary'
+						secureTextEntry
+						onChangeText={setPassword}
+					/>
 
 					<ForgotPasswordButton>
 						<ForgotPasswordLabel>
@@ -37,7 +53,12 @@ const SignIn = () => {
 						</ForgotPasswordLabel>
 					</ForgotPasswordButton>
 
-					<Button title="Entrar" type='secondary' />
+					<Button
+						title="Entrar"
+						type='secondary'
+						onPress={handleSignIn}
+						isLoading={isLogging}
+					/>
 				</Content>
 			</KeyboardAvoidingView>
 		</Container>
